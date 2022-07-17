356d55ae312ae2fb702c51e2c5a6a2bb0a6f57d0
norbertSviatko <informatikasviatko@gmail.com>
2022-07-17 21:06:45 +0200

    Add the variables stylesheet, add the custom fonts

---
+++ client/src/styles/variables.js
@@ -0,0 +1,27 @@
+const variables = {
+    "COLORS": {
+        "PRIMARY": "#624455",
+        "SECONDARY": "#9B6D97",
+        "TERTIARY": "#FFFFFF"
+    },
+    "FONT_SIZES": {
+        "GIANT": "36px",
+        "LARGE": "28px",
+        "MEDIUM": "22px",
+        "SMALL": "18px",
+        "MINI": "16px"
+    },
+    "FONT_FAMILIES": {
+        "MAIN": "Poppins",
+        "SIDE": "Aloja"
+    },
+    "BREAKPOINTS": {
+        "MOBILE": "",
+        "TABLET": "",
+        "LAPTOP": "",
+        "DESKTOP": "",
+        "LARGE": "",
+    }
+};
+
+export default variables;
