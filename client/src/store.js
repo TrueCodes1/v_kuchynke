1d1a4c7c82f842eb60172f0234e35525acd8e139
norbertSviatko <informatikasviatko@gmail.com>
2022-07-26 16:10:01 +0200

    (no big commit, fixing bugs on server to deploy the app to Heroku)

--- client/src/store.js
+++ client/src/store.js
@@ -3,8 +3,8 @@ import { combineReducers } from "redux";
 // IMPORTING REDUX REDUCERS
 // NOTE THAT THE NAMES OF IMPORTS ARE NOT THE SAME AS THE EXPORTS IN
 // THE REDUCERS
-import isDarkTheme from "./reducers/IsDarkTheme";
-import dataReleaseCountdown from "./reducers/DataReleaseCountdown";
+import isDarkTheme from "./reducers/IsDarkTheme.js";
+import dataReleaseCountdown from "./reducers/DataReleaseCountdown.js";
 
 // EXPORTING COMBINED REDUCER THAT HOLDS ALL
 // THE STATES OF THE WHOLE APP
