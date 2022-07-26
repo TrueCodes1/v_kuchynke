9ff0a63be7773ca638b5e442602f65db12376e3e
norbertSviatko <informatikasviatko@gmail.com>
2022-07-26 15:38:42 +0200

    Build up the basic server structure to deploy the app to the Heroku

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//This will create a middleware.
+//When you navigate to the root page, it would use the built react-app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(express.json());
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "/../client/build", "index.html"));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Listening on port " + port + "...");
+});
