f71f9a7d38c3248555eb933e51bcc819b9b5d79d
norbertSviatko <informatikasviatko@gmail.com>
2022-07-26 16:23:46 +0200

    (still fixing bugs on the server)

--- client/src/store.js
+++ client/src/store.js
@@ -3,8 +3,8 @@ import { combineReducers } from "redux";
 // IMPORTING REDUX REDUCERS
 // NOTE THAT THE NAMES OF IMPORTS ARE NOT THE SAME AS THE EXPORTS IN
 // THE REDUCERS
-import darkThemeReducer from "./reducers/IsDarkTheme";
-import countdownReducer from "./reducers/DataReleaseCountdown";
+import darkThemeReducer from "./redux_reducers/IsDarkTheme";
+import countdownReducer from "./redux_reducers/DataReleaseCountdown";
 
 // EXPORTING COMBINED REDUCER THAT HOLDS ALL
 // THE STATES OF THE WHOLE APP
