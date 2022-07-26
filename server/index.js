9b348718ef9baadaa5806e7685da562c8a399fdb
norbertSviatko <informatikasviatko@gmail.com>
2022-07-26 19:33:22 +0200

    will force the redirection to https instead of http

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+const secure = require("express-sslify");
 
 const app = express();
 
@@ -10,6 +11,14 @@ const __dirname = path.dirname(__filename);
 //This will create a middleware.
 //When you navigate to the root page, it would use the built react-app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+if (process.env.PORT) {
+  app.use(
+    secure.HTTPS({
+      trustProtoHeader: true,
+    })
+  );
+}
 
 // app.use(cors());
 // app.use(bodyParser.json());
